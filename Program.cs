ccc81b255d2994c488b51d15cf6c89df560500cc
cutealine <alina.carroll1558@gmail.com>
2024-09-29 23:57:09 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.ForegroundColor = ConsoleColor.Green; 
+Console.WriteLine("Я вас любил еще, быть может..."); 
+ 
+Console.WriteLine(""); 
+ 
+Console.ForegroundColor = ConsoleColor.Yellow; 
+Console.WriteLine("Я вас любил: любовь еще быть может,"); 
+Console.WriteLine("В душе моей угасла не совсем"); 
+Console.WriteLine("Но пусть она вас больше не треожит;"); 
+Console.WriteLine("Я не хочу печалить вас ничем."); 
+Console.WriteLine ("Я вас любил безмолвно, безнадежно,"); 
+Console.WriteLine ("То с робостью, то с ревностью томим;"); 
+Console.WriteLine ("Я вас любил так искренно, так нежно,"); 
+Console.WriteLine ("Как дай вам Бог любимой быть дургим."); 
+ 
+Console.WriteLine (""); 
+ 
+Console.ForegroundColor = ConsoleColor.Red; 
+Console.WriteLine ("А. С. Пушкин"); 
+Console.ResetColor ();
