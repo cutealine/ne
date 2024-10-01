8b396fac2e39ed1461b787785d5825571599a26f
cutealine <alina.carroll1558@gmail.com>
2024-10-01 11:11:17 +0500

    Add files via upload

---
+++ ProgramMat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину ребра тетраэдра:");
+        double edgeLength = double.Parse(Console.ReadLine());
+
+        double volume = Math.Pow(edgeLength, 3) / (6 * Math.Sqrt(2));
+        
+        double surfaceArea = Math.Sqrt(3) * Math.Pow(edgeLength, 2);
+
+        Console.WriteLine("Объем тетраэдра: " + Math.Round(volume, 3));
+        Console.WriteLine("Площадь поверхности тетраэдра: " + Math.Round(surfaceArea, 3));
+        Console.ReadKey();
+    }
+}
