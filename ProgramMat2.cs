fb3d170bdc3bc6bcde1da744ea034910eec7efd1
cutealine <alina.carroll1558@gmail.com>
2024-10-01 11:52:49 +0500

    Add files via upload

---
+++ ProgramMat2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число:");
+        string input = Console.ReadLine();
+
+        if (input.Length == 3 && int.TryParse(input, out int number))
+        {
+            string result = input.Substring(1) + input[0];
+            Console.WriteLine("Полученное число: " + result);
+        }
+        else
+        {
+            Console.WriteLine("Пожалуйста, введите корректное трехзначное число.");
+        }
+
+        Console.ReadKey();
+    }
+}
