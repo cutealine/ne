55e5a5b16f5d4a8f06da159df8ba42f335eaab4f
cutealine <alina.carroll1558@gmail.com>
2024-10-07 13:39:24 +0500

    Add files via upload

---
+++ ProgramTask4.1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+internal class Program{
+static void Main(string[] agrs){
+Console.WriteLine("Введите число x: ");
+var x = double.Parse(Console.ReadLine());
+
+var y = MyFuncion(x);
+Console.WriteLine("f(x) = " + y);
+
+Console.ReadKey();
+}
+
+static double MyFuncion(double x)
+{
+    //throw new NotImplementedException();
+    return Math.Sqrt((2*x + Math.Sin(3*x))/3.56);
+}
+}
