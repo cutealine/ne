4ec3a1c7f5bfd6565276a5aaf82778964b22d982
cutealine <alina.carroll1558@gmail.com>
2025-01-20 17:13:15 +0500

    Update ProgramTask10.1.cs

--- ProgramTask10.1.cs
+++ ProgramTask10.1.cs
@@ -1,1 +1,18 @@
-﻿
+using System;
+namespace project
+{
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число n: ");
+        int n = int.Parse(Console.ReadLine());
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += (double)i / (i + 1);
+        }
+        Console.WriteLine("Сумма: " + sum);
+    }
+}﻿
+}
