65e4b62178d75b8802bd15a4e14fa8847f94b288
cutealine <alina.carroll1558@gmail.com>
2025-01-20 17:15:25 +0500

    Update ProgramTask10.2.cs

--- ProgramTask10.2.cs
+++ ProgramTask10.2.cs
@@ -1,1 +1,24 @@
-﻿
+using System;
+namespace project
+{
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество элементов n: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] resistances = new double[n];
+        double totalResistance = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите сопротивление элемента {i+1}: ");
+            resistances[i] = double.Parse(Console.ReadLine());
+            totalResistance += 1 / resistances[i];
+        }
+
+        totalResistance = 1 / totalResistance;
+        Console.WriteLine("Общее сопротивление: " + totalResistance);
+    }
+}﻿
+}
