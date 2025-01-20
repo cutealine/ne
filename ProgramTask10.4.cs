d01a68f0d510c37120cf4f6685e673819a29d430
cutealine <alina.carroll1558@gmail.com>
2025-01-20 17:19:33 +0500

    Update ProgramTask10.4.cs

--- ProgramTask10.4.cs
+++ ProgramTask10.4.cs
@@ -1,1 +1,28 @@
+using System;
+namespace project
+{
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число n: ");
+        int n = int.Parse(Console.ReadLine());
 
+        Console.Write("Введите число k (0 <= k <= 8): ");
+        int k = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+            if (digit > k)
+            {
+                sum += digit;
+            }
+            n /= 10;
+        }
+        Console.WriteLine("Сумма цифр, больших " + k + ": " + sum);
+    }
+}
+}
