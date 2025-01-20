9c615f2196c4060b385de6d0d67a3fea8e0671ef
cutealine <alina.carroll1558@gmail.com>
2025-01-20 16:26:49 +0500

    Update ProgramTask6.2.cs

--- ProgramTask6.2.cs
+++ ProgramTask6.2.cs
@@ -1,18 +1,16 @@
 ﻿using System; 
- 
+
+namespace project
+{
 internal class Program 
 { 
     static void Main(string[] args) 
     { 
         string word = "апельсин"; 
- 
- 
         string word1 = word.Substring(1,2) + word.Substring(7, 1) + word.Substring(4, 1); 
- 
-     
-        string word2 = word.Substring(5,1) + word.Substring(1,1) + word.Substring(0,1) + word.Substring(7,1) + word.Substring(6,1) + word.Substring(2,3); 
- 
+        string word2 = word.Substring(5,1) + word.Substring(1,1) + word.Substring(0,1) + word.Substring(7,1) + word.Substring(6,1) + word.Substring(2,3);  
         Console.WriteLine("Первое слово: " + word1); 
         Console.WriteLine("Второе слово: " + word2); 
     } 
+}
 }
