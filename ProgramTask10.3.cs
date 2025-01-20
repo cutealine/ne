89648ac81c5899b99f8ccf1451a633f7e8cdc429
cutealine <alina.carroll1558@gmail.com>
2025-01-20 17:17:51 +0500

    Update ProgramTask10.3.cs

--- ProgramTask10.3.cs
+++ ProgramTask10.3.cs
@@ -1,1 +1,21 @@
+using System;
+namespace project
+{
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a (1 < a < 2): ");
+        double a = double.Parse(Console.ReadLine());
+        int n = 0;
+        double sum = 0;
 
+        while (sum <= a)
+        {
+            n++;
+            sum += 1.0 / Math.Pow(2, n);
+        }
+        Console.WriteLine("Наименьшее натуральное число n: " + n);
+    }
+}
+}
