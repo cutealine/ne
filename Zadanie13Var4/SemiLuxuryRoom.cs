2a718ec37fc598c2b82d2b6cb46ebb699c953846
cutealine <alina.carroll1558@gmail.com>
2025-06-05 23:03:19 +0500

    Add files via upload

---
+++ Zadanie13Var4/SemiLuxuryRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie14Var4
+{
+    public class SemiLuxuryRoom : Room
+    {
+        public string Amenities { get; set; }
+
+        public SemiLuxuryRoom(int number, int bedCount, WindowOrientation orientation, float priceForDay, string availableFrom, string amenities)
+            : base(number, bedCount, orientation, priceForDay, availableFrom)
+        {
+            Amenities = amenities;
+        }
+
+        public override string[] GetInfo()
+        {
+            var baseInfo = base.GetInfo();
+            var amenitiesInfo = $"Удобства: {Amenities}";
+
+            var fullInfo = new List<string>(baseInfo) { amenitiesInfo };
+            return fullInfo.ToArray();
+        }
+    }
+}
